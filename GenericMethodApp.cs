2afc05599901ceeef06fab6908d8668bcf71276c
hechul <113595306+hechul@users.noreply.github.com>
2022-12-03 20:39:46 +0900

    Add files via upload

--- GenericMethodApp.cs
+++ GenericMethodApp.cs
@@ -9,5 +9,6 @@ class GenericMethodApp{
     public static void Main(String[] args){
         int a = 1,b = 2;
         double c = 1.5 , d = 2.5;
+        Console.WriteLine("Before : a = {0}, b = {1}",a,b);
     }
 }
