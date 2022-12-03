ab8b4bdbbfd0254877a7f714d2d9f594b7ab782e
hechul <113595306+hechul@users.noreply.github.com>
2022-12-03 20:23:46 +0900

    Add files via upload

--- GenericMethodApp.cs
+++ GenericMethodApp.cs
@@ -1,7 +1,12 @@
 using System;
 class GenericMethodApp{
     static void Swap<DataType>(ref DataType x, ref DataType y){
-        
+        DataType temp;
+        temp = x;
+        x = y;
+        y = temp;
+    }
+    public static void Main(String[] args){
         
     }
 }
