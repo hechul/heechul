9c3104991a076b2d1534ca3ae66a32770411d488
hechul <113595306+hechul@users.noreply.github.com>
2022-12-03 19:24:12 +0900

    Add files via upload

--- PropertyApp.cs
+++ PropertyApp.cs
@@ -1,1 +1,9 @@
 using System;
+class Fraction{
+    private int numerator;
+    private ine denominator;
+    public int Numerator{
+        get {return numerator;}
+        set {numerator = value;}
+    }
+}
