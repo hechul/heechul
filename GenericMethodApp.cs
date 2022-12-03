598fb837c386b75f9b7a59699ce6f56780439952
hechul <113595306+hechul@users.noreply.github.com>
2022-12-03 23:12:55 +0900

    Add files via upload

--- GenericMethodApp.cs
+++ GenericMethodApp.cs
@@ -7,7 +7,7 @@ class GenericMethodApp{
         y = temp; // temp의 데이터를 y에 저장
     }
     public static void Main(String[] args){
-        int a = 1,b = 2;
+        int a = 1,b = 2; // a를 int 형 1, b를 int형 2로 정의함.
         double c = 1.5 , d = 2.5;
         Console.WriteLine("Before : a = {0}, b = {1}",a,b);
     }
