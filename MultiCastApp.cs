8eb053f7504dfeb0bb941ccfdf155e3eab6cdda0
hechul <113595306+hechul@users.noreply.github.com>
2022-12-03 20:07:19 +0900

    Add files via upload

--- MultiCastApp.cs
+++ MultiCastApp.cs
@@ -1,7 +1,13 @@
 using System;
-delegate void MultiCaseDelegate();
+delegate void MultiCastDelegate();
 class Schedule{
     public void Now(){
         Console.WriteLine("Time : " + DateTime.Now.ToString());
     }
+    public static void Today(){
+        Console.WriteLine("Date : " + DateTime.Today.ToString());
+    }
+}
+class MultiCastApp{
+
 }
