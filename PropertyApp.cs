8dadf338e037cbd58336933f80caa39573b17715
hechul <113595306+hechul@users.noreply.github.com>
2022-12-03 19:37:29 +0900

    Add files via upload

--- PropertyApp.cs
+++ PropertyApp.cs
@@ -20,7 +20,7 @@ class PropertyApp{
         int i=0;                     // Denominator에 값을 넣기위한 변수
         f.Numerator = 1;             //Numerator 프로퍼티를 이용해 1을 넣음
         i = f.Numerator + 1;         // Numerator 프로퍼티를 이용해 2를 받아옴
-        f.Denominator = i;
-        Console.WriteLine(f.ToString());
+        f.Denominator = i;           // Denominator 프로퍼티를 이용해 2를 넣음
+        Console.WriteLine(f.ToString()); //출력
     }
 }
