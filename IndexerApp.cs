ddaebd61dad0db5bba15e1b13b9ae79773ef79f6
hechul <113595306+hechul@users.noreply.github.com>
2022-12-03 19:59:53 +0900

    Add files via upload

--- IndexerApp.cs
+++ IndexerApp.cs
@@ -2,6 +2,12 @@ using System;
 class Color{
     private string[] color = new string[5];
     public string this[int index]{
+        get {return color[index];}
+        set {color[index] = value;}
+    }
+}
+class IndexrApp{
+    public static void Main(){
         
     }
 }
