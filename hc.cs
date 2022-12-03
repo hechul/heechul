3e8d9edb37ca200e28f0f5499e5c5aceb39e9954
hechul <113595306+hechul@users.noreply.github.com>
2022-12-03 19:17:21 +0900

    Add files via upload

---
+++ hc.cs
@@ -0,0 +1,20 @@
+using System;
+delegate void DelegateOne();
+delegate void DelegateTwo(int i);
+class DelegateClass{
+    public void MethodA(){
+        Console.WriteLine("In the DelegateClass.MethodA ...");
+    }
+    public void MethodB(int i){
+        Console.WriteLine("DelegateClass.MethodB, i = " + i);
+    }
+}
+class DelegateCallApp{
+    public static void Main(){
+        DelegateClass obj = new DelegateClass();
+        DelegateOne d1 = new DelegateOne(obj.MethodA);
+        DelegateTwo d2 = new DelegateTwo(obj.MethodB);
+        d1();
+        d2(20);
+    }
+}
