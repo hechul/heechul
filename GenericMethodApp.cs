1241cc99a25b4a3e595ffe87225a56ca19d648bc
hechul <113595306+hechul@users.noreply.github.com>
2022-12-04 11:47:26 +0900

    Add files via upload

--- GenericMethodApp.cs
+++ GenericMethodApp.cs
@@ -13,7 +13,7 @@ class GenericMethodApp{
         Swap<int>(ref a, ref b); // 정수형 변수로 호출
         Console.WriteLine("After : a = {0}, b = {1}",a,b);
         Console.WriteLine("Before : c = {0}, d = {1}",c,d);
-        Swap<double>(ref c, ref d);
+        Swap<double>(ref c, ref d); // 실수형 변수로 호출
         Console.WriteLine("After : c = {0}, d = {1}",c,d);
     	}
 }
