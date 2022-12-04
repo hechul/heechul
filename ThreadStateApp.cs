c5cb42fe595ef683439b2744fdc1b02ce1be8003
hechul <113595306+hechul@users.noreply.github.com>
2022-12-04 20:33:45 +0900

    Add files via upload

---
+++ ThreadStateApp.cs
@@ -0,0 +1,23 @@
+using System;
+using System.Threading;
+class ThreadState{
+    public void ThreadBody(){
+        while (true){
+            // ... infinite loop ...
+        }
+    }
+}
+class ThreadStateApp{
+    public static void Main(String[] args){
+        ThreadState obj = new ThreadState();
+        ThreadStart ts = new ThreadStart(obj.ThreadBody);
+        Thread t = new Thread(ts);
+        Console.WriteLine("Step 1: " + t.ThreadState);
+        t.Start();
+        Thread.Sleep(100);
+        Console.WriteLine("Step 2: " + t.ThreadState);
+        t.suspend();
+        Thread.Sleep(100);
+        C
+    }
+}
