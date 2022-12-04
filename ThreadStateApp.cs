79d3beecd3cfa3235067cd0be3a6f8b28f54d6d8
hechul <113595306+hechul@users.noreply.github.com>
2022-12-04 20:30:28 +0900

    Add files via upload

--- ThreadStateApp.cs
+++ ThreadStateApp.cs
@@ -12,6 +12,6 @@ class ThreadStateApp{
         ThreadState obj = new ThreadState();
         ThreadStart ts = new ThreadStart(obj.ThreadBody);
         Thread t = new Thread(ts);
-        Console.WriteLine("Step 1:")
+        Console.WriteLine("Step 1: "+t.ThreadState);
     }
 }
