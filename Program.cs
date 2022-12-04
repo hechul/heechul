fc72e1c0fdca5b9205889d4e9114868eabf79c21
hechul <113595306+hechul@users.noreply.github.com>
2022-12-04 19:19:45 +0900

    Add files via upload

--- Program.cs
+++ Program.cs
@@ -3,7 +3,7 @@ class Program{
     public static void Main(String[] args){
         Thread myThread = new Thread(Func);
         myThread.Start(7);
-        for(int i=0;)
+        for(int i=0;i<3;)
     
     }
 }
