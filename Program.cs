091e401e5221755731f98783002d532d0402c736
hechul <113595306+hechul@users.noreply.github.com>
2022-12-04 19:17:54 +0900

    Add files via upload

--- Program.cs
+++ Program.cs
@@ -2,5 +2,8 @@ using System;
 class Program{
     public static void Main(String[] args){
         Thread myThread = new Thread(Func);
+        myThread.Start(7);
+        for(int i=0;)
+    
     }
 }
