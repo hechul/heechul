e9313a2bafb9c7e36e109344c7f0574e31d754b5
hechul <113595306+hechul@users.noreply.github.com>
2022-12-04 18:31:04 +0900

    Add files via upload

--- SystemHandlerApp.cs
+++ SystemHandlerApp.cs
@@ -3,7 +3,8 @@ class SystemHandlerApp{
  public static void Main(String[] args){
     int[] vector = {1,2,3} // 인덱스 선언
     try{
-      Console.Writeline("")
+      Console.Writeline("Here : 1");
+      vector[4] = 4;
     }
  }
 }
