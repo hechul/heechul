8de59e22aeb9fcdc713776a1d3d7045540944cd7
hechul <113595306+hechul@users.noreply.github.com>
2022-12-04 14:57:44 +0900

    Add files via upload

--- IndexerApp.cs
+++ IndexerApp.cs
@@ -1,7 +1,7 @@
 using System;
 class Color{ // 컬러 클래스
     private string[] color = new string[5]; // 5개의 컬러를 넣데위한 인데스
-    public string this[int index]{
+    public string this[int index]{ // 인덱스를 위한 프로퍼티
         get {return color[index];}
         set {color[index] = value;}
     }
