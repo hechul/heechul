1c287dac8e965d0bfaca107647d9da14101e8dea
hechul <113595306+hechul@users.noreply.github.com>
2022-12-04 14:47:20 +0900

    Add files via upload

--- UserExceptionApp.cs
+++ UserExceptionApp.cs
@@ -2,7 +2,7 @@ using System;
 class UserErrExeption:ApplicationException{
     public UserErrExeption(string s) : base(s){}
 }
-class UserException{
+class UserException{ //
     public static void Main(String[] args){
         try{
             throw new UserErrExeption("throw a excepction with a message");
