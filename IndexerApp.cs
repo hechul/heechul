7fd84bcfd4fe7de0dc67fd92bada7d19c6f0ce76
hechul <113595306+hechul@users.noreply.github.com>
2022-12-04 14:56:15 +0900

    Add files via upload

--- IndexerApp.cs
+++ IndexerApp.cs
@@ -8,7 +8,7 @@ class Color{ // 컬러 클래스
 }
 class IndexrApp{ // 인덱스 클래스
     public static void Main(){
-        Color c = new Color();
+        Color c = new Color(); // 생성자
         c[0] = "WHITE";
         c[1] = "RED";
         c[2] = "YELLOW";
